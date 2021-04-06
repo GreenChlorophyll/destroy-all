55a7efcf19c03f1c553a3fd7a43fcf57da7704fd
CrownPointTMD <69466128+CrownPointTMD@users.noreply.github.com>
2021-04-06 15:55:24 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-for _,v in pairs(workspace.bla:GetDescendants()) do if v:isA(model) then v:Destory() end end  
+for _,v in pairs(workspace:GetDescendants()) do if v:isA(model) then v:Destory() end end  
